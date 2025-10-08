23ba6406f9d58630833cb9ebccfed86478aecd66
J <68155263+jxxxxe@users.noreply.github.com>
2025-10-08 10:26:51 +0900

    feat: add interactive prompt for selecting design pattern

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,6 @@
 import { Command } from "commander";
 import { runGenerator } from "./main";
+import { confirm, select } from "@inquirer/prompts";
 
 const program = new Command();
 
@@ -7,15 +8,50 @@ program
   .name("create-patterned-app")
   .description("Scaffold a frontend project based on design patterns")
   .argument("<project-name>", "Name of your project")
-  .option(
-    "--pattern <type>",
-    "Project structure pattern (atomic or fsd)",
-    "atomic"
-  )
+  .option("--pattern <type>", "Project structure pattern")
   .option("--ts", "Use TypeScript", false)
   .option("--react", "Set up for React", false)
-  .action((projectName: string, options: any) => {
-    runGenerator(projectName, options);
+  .option("--only-dir", "Only create folder structure", false)
+  .action(async (projectName: string, optionsFromCLI: any) => {
+    let pattern = optionsFromCLI.pattern;
+    if (!pattern) {
+      const patternAnswer = await select({
+        message: "Which design pattern would you like to use?",
+        choices: [
+          { name: "Atomic Design", value: "atomic" },
+          { name: "Feature-Sliced Design (FSD)", value: "fsd" },
+          {
+            name: "Atomic Design + Feature-Sliced Design (FSD)",
+            value: "atomic+fsd",
+          },
+        ],
+      });
+      pattern = patternAnswer;
+    }
+
+    const { ts, react, onlyDir } = {
+      ts: await confirm({
+        message: "Do you want to use TypeScript?",
+        default: false,
+      }),
+      react: await confirm({
+        message: "Is this a React project?",
+        default: false,
+      }),
+      onlyDir: await confirm({
+        message: "Generate only folder structure (no files)?",
+        default: false,
+      }),
+    };
+
+    const options = {
+      pattern,
+      ts: optionsFromCLI.ts ?? ts,
+      react: optionsFromCLI.react ?? react,
+      onlyDir: optionsFromCLI.onlyDir ?? onlyDir,
+    };
+
+    await runGenerator(projectName, options);
   });
 
 program.parse();
