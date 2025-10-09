b8439d57f48ae294210638552e55897f2340a161
J <68155263+jxxxxe@users.noreply.github.com>
2025-10-09 20:35:02 +0900

    code: rename project

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
-# create-patterned-app
+# create-folder-structure
 
 🛠 A CLI tool that scaffolds frontend project structures based on design patterns
 
 ## Getting Started
 
 ```bash
-npx create-patterned-app my-app --pattern atomic --ts --react
+npx create-folder-structure .
 ```
 
 ## Features
 
 • Supports Atomic, FSD, and more<br />
 • TypeScript / JavaScript selectable<br />
-• React / Vanilla optional<br />
-• Pre-configured folder structure<br />
--- src/cli.ts
+++ src/cli.ts
@@ -5,12 +5,11 @@ import { confirm, select } from "@inquirer/prompts";
 const program = new Command();
 
 program
-  .name("create-patterned-app")
+  .name("create-folder-structure")
   .description("Scaffold a frontend project based on design patterns")
   .argument("<project-name>", "Name of your project")
   .option("--pattern <type>", "Project structure pattern")
-  .option("--ts", "Use TypeScript", false)
-  .option("--react", "Set up for React", false)
+  .option("--ts", "Use TypeScript", true)
   .option("--only-dir", "Only create folder structure", false)
   .action(async (projectName: string, optionsFromCLI: any) => {
     const patterns = ["atomic", "fsd", "atomic+fsd"];
@@ -30,32 +29,22 @@ program
         choices: [
           { name: "Atomic Design", value: "atomic" },
           { name: "Feature-Sliced Design (FSD)", value: "fsd" },
-          {
-            name: "Atomic Design + Feature-Sliced Design (FSD)",
-            value: "atomic+fsd",
-          },
         ],
       });
       pattern = patternAnswer;
     }
 
-    const { ts, react, onlyDir } = {
+    const { ts, onlyDir } = {
       ts:
         optionsFromCLI.ts ??
         (await confirm({
           message: "Do you want to use TypeScript?",
           default: false,
         })),
-      react:
-        optionsFromCLI.react ??
-        (await confirm({
-          message: "Is this a React project?",
-          default: false,
-        })),
       onlyDir:
         optionsFromCLI.onlyDir ??
         (await confirm({
-          message: "Generate only folder structure (no files)?",
+          message: "Generate only folder structure (no example files)?",
           default: false,
         })),
     };
@@ -63,7 +52,6 @@ program
     const options = {
       pattern,
       ts,
-      react,
       onlyDir,
     };
 
